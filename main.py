3dce4b3d63b112bf9b8e1d79722371aaba87f4f6
Pedro Alonso <pedro.alonsodeoliveira@hotmail.com>
2024-11-05 15:04:53 -0300

    feat: added plot graphics to main program

--- main.py
+++ main.py
@@ -2,35 +2,57 @@ import os
 
 from interfaces.english_interface import run_english_interface
 from interfaces.portuguese_interface import run_portuguese_interface
+from data.graphics import run_graphics_app
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+program_functionality = 0
 language = 0
-suported_languages = 2
+supported_languages = 2
 
-while language < 1 or language > suported_languages:
+while program_functionality != (1 or 2):
     print("""
             By Daniel Servejeira & Pedro Alonso\n
-            Select your language: 
-            [1] <- English (Coming soon...)
-            [2] <- Portuguese\n
-        """)
-
+            The program will:
+            [1] <- Show sorting algorithms graphics
+            [2] <- Let you test sorting algorithms\n
+          """)
     try:
-        language = int(input("Enter your choice: "))
-    except ValueError:
+        program_functionality = int(input("Enter your choice: "))
+    except ValueError:  
         print("Please enter a valid number.\n")
         clear_screen()
-        continue
+        
+    if program_functionality == 1:
+        clear_screen()
+        run_graphics_app()
+        
+    elif program_functionality == 2:
+        while language < 1 or language > supported_languages:
+            print("""
+                    
+                    Select your language: 
+                    [1] <- English (Coming soon...)
+                    [2] <- Portuguese\n
+                """)
 
-if language == 1:
-    input("Press Enter to continue...")
-    clear_screen()
-    run_english_interface()
-elif language == 2:
-    input("Pressione Enter para continuar...")
-    clear_screen()
-    run_portuguese_interface()
+            try:
+                language = int(input("Enter your choice: "))
+
+                if language == 1:
+                    input("Press Enter to continue...")
+                    clear_screen()
+                    run_english_interface()
+                elif language == 2:
+                    input("Pressione Enter para continuar...")
+                    clear_screen()
+                    run_portuguese_interface()
+            
+            except ValueError:
+                print("Please enter a valid number.\n")
+                clear_screen()
 
 clear_screen()
+
+
